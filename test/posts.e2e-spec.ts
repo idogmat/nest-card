5cb4f0786e6dc5430a37161ae255b823687332f0
idogmat <idogmat@gmail.com>
2024-09-11 20:00:59 +0300

    add test post

--- test/posts.e2e-spec.ts
+++ test/posts.e2e-spec.ts
@@ -68,7 +68,6 @@ describe('posts', () => {
   });
 
   it('should update post', async () => {
-
     const posts = await request(app.getHttpServer())
       .post(`/blogs/${blogId}/posts`)
       .auth(login, password, { type: "basic" })
@@ -90,45 +89,65 @@ describe('posts', () => {
     expect(getPost.body.title).toBe('newww');
   });
 
-  // it('should delete blog', async () => {
-  //   const result = await request(app.getHttpServer())
-  //     .post('/blogs')
-  //     .auth(login, password, { type: "basic" })
-  //     .send(newBlog);
-  //   expect(result.status).toBe(201);
+  it('should delete post', async () => {
+    const post = await request(app.getHttpServer())
+      .post(`/blogs/${blogId}/posts`)
+      .auth(login, password, { type: "basic" })
+      .send(newPost);
+    expect(post.status).toBe(201);
+    expect(post.body.blogId).toBe(blogId);
 
-  //   const blog = await request(app.getHttpServer())
-  //     .delete(`/blogs/${result.body.id}`)
-  //     .auth(login, password, { type: "basic" })
-  //     .send();
-  //   expect(blog.status).toBe(204);
+    const result = await request(app.getHttpServer())
+      .delete(`/posts/${post.body.id}`)
+      .auth(login, password, { type: "basic" })
+      .send();
+    expect(result.status).toBe(204);
 
-  //   const blogExist = await request(app.getHttpServer())
-  //     .get(`/blogs/${result.body.id}`)
-  //     .send();
-  //   expect(blogExist.status).toBe(404);
-  // });
+    const postExist = await request(app.getHttpServer())
+      .get(`/posts/${post.body.id}`)
+      .send();
+    expect(postExist.status).toBe(404);
+  });
 
-  // it('should update blog', async () => {
-  //   const result = await request(app.getHttpServer())
-  //     .post('/blogs')
-  //     .auth(login, password, { type: "basic" })
-  //     .send(newBlog);
-  //   expect(result.status).toBe(201);
+  it('should add like', async () => {
+    const post = await request(app.getHttpServer())
+      .post(`/blogs/${blogId}/posts`)
+      .auth(login, password, { type: "basic" })
+      .send(newPost);
+    expect(post.status).toBe(201);
+    expect(post.body.blogId).toBe(blogId);
 
-  //   const blogUpdated = await request(app.getHttpServer())
-  //     .put(`/blogs/${result.body.id}`)
-  //     .auth(login, password, { type: "basic" })
-  //     .send({
-  //       name: "yep",
-  //     });
-  //   expect(blogUpdated.status).toBe(204);
+    // auth
+    const regUser = { login: 'name77', password: 'qwerty1221', email: 'email3787@gil.em' };
 
-  //   const blog = await request(app.getHttpServer())
-  //     .get(`/blogs/${result.body.id}`)
-  //     .send();
-  //   expect(blog.status).toBe(200);
-  //   expect(blog.body.name).toBe('yep');
+    const authResult = await request(app.getHttpServer())
+      .post('/auth/registration')
+      .send(regUser);
+    expect(authResult.status).toBe(204);
 
-  // });
+    const loginRequest = await request(app.getHttpServer())
+      .post('/auth/login')
+      .send({ loginOrEmail: regUser.login, password: regUser.password });
+    expect(loginRequest.status).toBe(200);
+    expect(loginRequest.body).toHaveProperty('accessToken');
+
+    const accessToken: string = loginRequest.body?.accessToken.toString();
+    // like post
+    const postLike = await request(app.getHttpServer())
+      .put(`/posts/${post.body.id}/like-status`)
+      .set({ Authorization: "Bearer " + accessToken })
+      .send({
+        likeStatus: "Like"
+      });
+    expect(postLike.status).toBe(204);
+
+    const postLiked = await request(app.getHttpServer())
+      .get(`/posts/${post.body.id}`)
+      .set({ Authorization: "Bearer " + accessToken })
+      .send();
+
+    expect(postLiked.status).toBe(200);
+    expect(postLiked.body.extendedLikesInfo.myStatus).toBe("Like");
+    expect(postLiked.body.extendedLikesInfo.likesCount).toBe(1);
+  });
 });
